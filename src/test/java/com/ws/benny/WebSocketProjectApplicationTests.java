241e82c69fd2f799a81bfc6a9aa030590cefea7b
wangbinbin <wang951226@163.com>
2023-03-14 15:52:59 +0800

    初次提交

---
+++ src/test/java/com/ws/benny/WebSocketProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.benny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
